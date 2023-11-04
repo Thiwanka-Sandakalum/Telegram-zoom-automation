2cd9fcaabfdff875cccb9bc56e2b7f64487370c2
Thiwanka-Sandakalum <thiwanka2002sandakalum@gmail.com>
2023-11-04 15:29:14 +0530

    ...

--- server/services/zoom.js
+++ server/services/zoom.js
@@ -9,6 +9,8 @@ const botToken = process.env.TOKENBOT;
 let accessToken = null;
 const bot = new TelegramBot(botToken, { polling: true });
 const chatid = process.env.ALLOWED_CHAT_ID
+bot.sendMessage(chatid, 'Hello from your Telegram bot!')
+
 
 async function getRecordings() {
     console.log('getRecordings')
@@ -71,6 +73,8 @@ async function getRecordings() {
 async function uploadTelegram(downloadUrl) {
     try {
         console.log("uploadTelegram")
+        bot.sendMessage(chatid, "uploadTelegram")
+
         // const downloadUrl = "https://dl109.dlmate13.online/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ0xqY0UreUI4eFNveFpxTVlDMXZhckp0MWNaYWdKaHF1a0Z0WlYremZMWmRHRmV6Q2Y1TXgyY2oyTTU0Sjd2emJEOXJFd1dOMTVDMU9xdmYrc2d5Vmppd0t3TFA3QUJld1RQMUY5NWhKRnlSYTMvdERBbmhLdmhWU1NzUi9YUEJSRXZ5dGZlL0xWL3NKay9pL09kK2Z0d1pVUnBDdWF2OHNmZzZuTjdGU3ZrdWQ0cThvb1drWWxJUT09"
         axios({
             method: 'get',
@@ -78,8 +82,9 @@ async function uploadTelegram(downloadUrl) {
             responseType: 'stream',
         })
             .then(response => {
-                console.log(response.data)
+                console.log('downlooading')
                 bot.sendVideo(chatid, response.data);
+                console.log('uploading finished')
             })
             .catch(error => {
                 console.error('Error downloading and uploading the video:', error);
